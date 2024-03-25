57d51225acf90e7ca66d059ba978df0bde029df1
VShreeKrishna <vshreekrishna2110@gmail.com>
2024-03-26 01:56:55 +0530

    modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#001233' }}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/" style={{ textAlign: 'center', color: '#ffffff', fontWeight:'bold' }}>iNotebook</Link>
+                <Link className="navbar-brand" to="/" style={{ textAlign: 'center', color: '#ffffff', fontWeight:'bold' }}>iNoteBook</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
